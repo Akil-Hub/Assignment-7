f600c22745f07244cf9dd731993d6f330847bd1d
Akil-Hub <akilanjummahi@gmail.com>
2026-04-13 23:39:15 +0600

    complete the banner section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
+import Banner from "@/components/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
  <>
  <article className="bg-body h-screen">
- <h2>welcome Home </h2>
+ <Banner/>
 
  </article>
  </>
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { FaTextWidth } from "react-icons/fa";
 
 const Banner = () => {
   return (
-    <section className='wrapper text-center'>
-        Friends to keep close in your life       
-    </section>
-  )
-}
+    <section className="wrapper text-center">
+      <h1 className="text-4xl font-bold text-gray-800 ">
+        Friends to keep close in your life
+      </h1>
+      <p className="text-secondary w-130 mx-auto py-3">
+        Your personal shelf of meaningful connections. Browse, tend, and nurture
+        the relationships that matter most.
+      </p>
+      <Button className={'text-white py-3'} size="sm">+ Add a Friend</Button>
 
-export default Banner
+      <div className="grid grid-cols-2 lg:grid-cols-4 wrapper  justify-items-center gap-x-7 py-10 border-b border-gray-200">
+
+        <div className=" bg-white text-center py-4 rounded-md w-full">
+            <h4 className="text-xl text-primary font-semibold">10</h4>
+
+            <p className="text-secondary ">Total Friends</p>
+        </div>
+        <div className=" bg-white text-center py-4 ro unded-md w-full">
+            <h4 className="text-xl text-primary font-semibold">3</h4>
+
+            <p className="text-secondary ">On Track</p>
+        </div>
+        <div className=" bg-white text-center py-4 rounded-md w-full">
+            <h4 className="text-xl text-primary font-semibold">6</h4>
+
+            <p className="text-secondary ">Need Attention</p>
+        </div>
+        <div className=" bg-white text-center py-4 rounded-md w-full">
+            <h4 className="text-xl text-primary font-semibold">12</h4>
+
+            <p className="text-secondary ">Interactions This Month  </p>
+        </div>
+      </div>
+
+
+    </section>
+  );
+};
+
+export default Banner;
